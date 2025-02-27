d6939c59ba31969ff14d94816df5a017e1ad00e5
Vittorio Vittori <vittorio.vittori@maggioli.it>
2025-02-27 16:19:17 +0100

    update configs

---
+++ iconsauce.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,json,ts,tsx}',
+  ],
+  plugin: [
+    require('@iconsauce/material-icons'),
+    require('@iconsauce/mdi-svg'),
+    require('@iconsauce/mgg-icons'),
+  ],
+}
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,10 @@
 import { Config } from "@stencil/core";
 import { postcss } from "@stencil-community/postcss";
+import alias from '@rollup/plugin-alias'
+import { inlineSvg } from 'stencil-inline-svg'
 import autoprefixer from "autoprefixer";
 import tailwind from "tailwindcss";
+import path from 'path';
 
 export const config: Config = {
   namespace: "stencil-starter-project-name",
@@ -10,6 +13,21 @@ export const config: Config = {
     postcss({
       plugins: [autoprefixer({ flexbox: "no-2009" }), tailwind()],
     }),
+    alias({
+      entries: [
+        { find: /^@common\/(.*)$/, replacement: path.resolve('.', './src/common/$1') },
+        { find: /^@component\/(.*)$/, replacement: path.resolve('.', './src/components/$1') },
+        { find: /^@dictionary\/(.*)$/, replacement: path.resolve('.', './src/dictionary/$1') },
+        { find: /^@event\/(.*)$/, replacement: path.resolve('.', './src/event-detail/$1') },
+        { find: /^@fixture\/(.*)$/, replacement: path.resolve('.', './src/fixtures/$1') },
+        { find: /^@meta\/(.*)$/, replacement: path.resolve('.', './src/meta/$1') },
+        { find: /^@icon\/([a-zA-Z-\/]+)\.svg$/, replacement: path.resolve(__dirname, './public/assets/svg/$1.svg') },
+        { find: /^@tailwind\/(.*)$/, replacement: path.resolve('.', './src/tailwind/$1') },
+        { find: /^@test\/(.+)$/, replacement: path.resolve('.', './src/test/$1') },
+        { find: /^@type\/(.+)$/, replacement: path.resolve('.', './src/type/$1') },
+      ],
+    }),
+    inlineSvg(),
   ],
   outputTargets: [
     {
